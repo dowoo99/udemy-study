b0a60a635366b13d6e3085af6b87d990320f335e
dowoo99 <dkalstn12@naver.com>
2022-11-29 20:43:46 +0900

    오류수정

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,40 +1,59 @@
 import React, { useState } from "react";
+
 import "./ExpenseForm.css";
+
 const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
-  const [enterdeAmount, setEnteredAmount] = useState("");
-  const [enterdeDate, setEnteredDate] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
   // const [userInput, setUserInput] = useState({
-  //   enteredTitle: "",
-  //   enteredAmount: "",
-  //   enteredDate: "",
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: '',
   // });
-  const titleChangeHandler = (e) => {
-    setEnteredTitle(e.target.value);
-    // setUserInput((prev) => {
-    //   return { ...prev, enteredTitle: e.target.value };
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
     // });
   };
-  const amountChangeHandler = (e) => {
-    setEnteredAmount(e.target.value);
-    // setUserInput({ ...userInput, enteredAmount: e.target.value });
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
   };
-  const dateChangeHandler = (e) => {
-    setEnteredDate(e.target.value);
-    // setUserInput({ ...userInput, enteredDate: e.target.value });
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
   };
-  const submitHandler = (e) => {
-    e.preventDefault();
-    const expenseDate = {
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
       title: enteredTitle,
-      amount: enterdeAmount,
-      date: new Date(enterdeDate),
+      amount: enteredAmount,
+      date: new Date(enteredDate),
     };
-    props.onSaveExpenseData(expenseDate);
+
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
   };
+
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
@@ -49,20 +68,20 @@ const ExpenseForm = (props) => {
         <div className="new-expense__control">
           <label>Amount</label>
           <input
-            value={enterdeAmount}
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
           <input
-            value={enterdeDate}
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
@@ -73,4 +92,5 @@ const ExpenseForm = (props) => {
     </form>
   );
 };
+
 export default ExpenseForm;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,22 @@
 import React from "react";
+
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
+
 const NewExpense = (props) => {
-  const saveExpenseDateHandler = (enteredExpenseData) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
   };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseDate={saveExpenseDateHandler} />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
+
 export default NewExpense;
